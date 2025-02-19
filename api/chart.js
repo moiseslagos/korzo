fef27b7c59809b06d7db867de8ba4c5f6efbcc98
moiseslagos <moises180688@gmail.com>
2025-02-19 13:16:49 -0500

    update charts page

--- api/chart.js
+++ api/chart.js
@@ -1,19 +1,12 @@
-/* eslint-disable import/no-extraneous-dependencies */
-import express from "express";
-import fetch from "node-fetch";
-
-const app = express();
-
-app.get("/api/chart", async (req, res) => {
+export default async function handler(req, res) {
   try {
     const response = await fetch(
       "https://query1.finance.yahoo.com/v8/finance/chart/AAPL?range=1y&interval=1mo"
     );
     const data = await response.json();
-    res.json(data);
+    res.setHeader("Access-Control-Allow-Origin", "*"); // Evita problemas de CORS
+    res.status(200).json(data);
   } catch (error) {
     res.status(500).json({ error: "Error fetching stock data" });
   }
-});
-
-export default app;
+}
