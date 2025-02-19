3c74b3bb9453c6c94b04bdd2d5ab35a31580a04e
moiseslagos <moises180688@gmail.com>
2025-02-19 13:14:20 -0500

    update charts page

--- api/chart.js
+++ api/chart.js
@@ -1,11 +1,19 @@
-export default async function handler(req:any, res: any) {
+/* eslint-disable import/no-extraneous-dependencies */
+import express from "express";
+import fetch from "node-fetch";
+
+const app = express();
+
+app.get("/api/chart", async (req, res) => {
   try {
     const response = await fetch(
       "https://query1.finance.yahoo.com/v8/finance/chart/AAPL?range=1y&interval=1mo"
     );
     const data = await response.json();
-    res.status(200).json(data);
+    res.json(data);
   } catch (error) {
     res.status(500).json({ error: "Error fetching stock data" });
   }
-}
+});
+
+export default app;
